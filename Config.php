00be77d300bf12a91dbc30a1875856aa3fc4daf1
Y-Smail <32673699+Y-Smail@users.noreply.github.com>
2018-03-28 15:13:40 +0200

    Update Config.php

--- Config.php
+++ Config.php
@@ -2,7 +2,7 @@
 session_start();
 try{
    $pdo_options[PDO::ATTR_ERRMODE]=PDO::ERRMODE_EXCEPTION;
-   $db=new PDO('mysql:host==mademoiselle-mysqldbserver.mysql.database.azure.com; dbname=blog','mysqldbuser@mademoiselle-mysqldbserver','Estiam1994');
+   $db=new PDO('mysql:host=mademoiselle-mysqldbserver.mysql.database.azure.com; dbname=blog','mysqldbuser@mademoiselle-mysqldbserver','Estiam1994');
    $db->setAttribute(PDO::ATTR_CASE, PDO::CASE_LOWER); // mettre les noms des champs en maj
    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
 }
